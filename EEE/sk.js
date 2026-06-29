90218c159141c5df91c9c427dd23f66bc05c75b6
IT999555 <titanfnaf7283@gmail.com>
2026-06-29 16:40:54 +0700

    Add files via upload

---
+++ EEE/sk.js
@@ -0,0 +1,11 @@
+for(i=1;i<5;i++){
+document.write("<h",i,">cs cs cs @ ih hitler </h",i,">") ;
+}
+num = 1 ;
+for(i=1;i<=10;i++){
+    for(j=1;j<=num;j++){
+        document.write("<img src='Screenshot 2026-06-13 125450.png'width=100>" ) ;   
+    }
+    num++ ;
+    document.write("<br>");
+}
